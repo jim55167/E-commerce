9a9fb804c5ad0fda75e2d5b12bcd6105b31603da
Jun <j405082003@gmail.com>
2020-10-24 02:21:51 +0800

    新增產品與刪除

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -67,7 +67,7 @@
                     <i class="fas fa-spinner fa-spin"></i>
                 </label>
                 <input type="file" id="customFile" class="form-control"
-                    ref="files">
+                    ref="files" @change="uploadFile">
                 </div>
                 <img img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=828346ed697837ce808cae68d3ddc3cf&auto=format&fit=crop&w=1350&q=80"
                 class="img-fluid" alt="" :src="tempProduct.imageUrl">
@@ -225,13 +225,30 @@ export default {
       });
     },
     deleteProduct() {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/product${this.tempProduct.id}`
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/product${this.tempProduct.id}`;
       this.$http.delete(api, { data:this.tempProduct }).then((response) => {
         if(response.data.success) {
           $('#delProductModal').modal('hide');
           this.getProducts();
         }
       });
+    },
+    uploadFile() { //上傳圖片
+      console.log(this);
+      const uploadedFile = this.$refs.files.files[0];
+      const formData = new FormData();
+      formData.append('file-to-upload', uploadedFile);
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/upload`;
+      this.$http.post(url, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((response) => {
+        console.log(response.data);
+        if(response.data.success) {
+          this.$set(this.tempProduct, 'imageUrl', response.data.imageUrl);
+        }
+      })
     },
   },
   created() {
